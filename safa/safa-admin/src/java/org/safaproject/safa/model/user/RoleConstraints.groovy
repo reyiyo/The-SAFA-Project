aa7e980c62bd1031058c78ef9cb2d9cf80e4c82f
Demian Fuentes <fuentes.demian@gmail.com>
2012-06-25 01:24:18 -0300

    scaffolding Role, faltan los test que van en el proximo commit

---
+++ safa/safa-admin/src/java/org/safaproject/safa/model/user/RoleConstraints.groovy
@@ -0,0 +1,3 @@
+constraints = {
+	name(nullable: false, unique:true/*, inList: [Roles.ROLE_ADMIN, Roles.ROLE_MOD, Roles.ROLE_USER]*/)
+}
---
+++ safa/safa-admin/test/unit/org/safaproject/safa/model/user/RoleControllerTests.groovy
@@ -0,0 +1,17 @@
+package org.safaproject.safa.model.user
+
+import grails.test.*
+
+class RoleControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
